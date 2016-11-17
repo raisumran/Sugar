4323e45dfe0555ea02700ef5d4fe23ff0749d06e
raisumran <sumranrai@hotmail.com>
2016-11-17 04:36:24 -0800

    new field created

---
+++ SugarPro7.6/custom/modules/Cases/Ext/Language/en_us.lang.ext.php
@@ -0,0 +1,7 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+
+//Merged from custom/Extension/modules/Cases/Ext/Language/en_us.lang.php
+
+// WARNING: The contents of this file are auto-generated.
+$mod_strings['LBL_SAM'] = 'sam';
